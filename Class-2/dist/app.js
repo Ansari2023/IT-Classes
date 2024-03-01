781ae51984c0634da1eee1ce3e2e0f429e692947
ansari2023 <imranahmedansari2021@gmail.com>
2024-03-02 02:23:57 +0500

    new files

--- Class-2/dist/app.js
+++ Class-2/dist/app.js
@@ -1,2 +1,6 @@
-var username = "Imran Ahmed";
-console.log(username);
+// let userName: string= "Imran Ahmed";
+// console.log(userName);
+var myIntro = "Imran";
+myIntro = "Ahmed";
+myIntro = "Ansari";
+console.log(myIntro);
--- Class-2/src/app.ts
+++ Class-2/src/app.ts
@@ -1,2 +1,6 @@
-let userName: string= "Imran Ahmed";
-console.log(userName);
+// let userName: string= "Imran Ahmed";
+// console.log(userName);
+let myIntro = "Imran";
+myIntro = "Ahmed";
+myIntro = "Ansari";
+console.log (myIntro);
