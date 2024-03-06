33a8c11c63f3afed104a1c4f6570c6700d560916
ansari2023 <imranahmedansari2021@gmail.com>
2024-03-07 02:02:58 +0500

    update

--- Class-3/app.ts
+++ Class-3/app.ts
@@ -1,6 +1,16 @@
 //variable differences between let and Var. 
 
-let Imran: string = "Class-1";
-Imran = "class-2";
-Imran = "class-3";
-console.log(Imran);
+// let Imran: string = "Class-1";
+// Imran = "class-2";
+// Imran = "class-3";
+// console.log(Imran);
+
+// let sunny = "John";
+// let sunny = "Peter";
+// console.log(sunny)
+// console.log(sunny)
+
+var sunny = "John";
+var sunny = "Peter";
+console.log(sunny)
+console.log(sunny)
