1aff155763614b9386a2182bd40d484b01655a49
ansari2023 <imranahmedansari2021@gmail.com>
2024-03-07 01:51:27 +0500

    add

---
+++ Class-3/app.js
@@ -0,0 +1,5 @@
+//variable differences between let and Var. 
+var Imran = "Class-1";
+Imran = "class-2";
+Imran = "class-3";
+console.log(Imran);
---
+++ Class-3/app.ts
@@ -0,0 +1,6 @@
+//variable differences between let and Var. 
+
+let Imran: string = "Class-1";
+Imran = "class-2";
+Imran = "class-3";
+console.log(Imran);
