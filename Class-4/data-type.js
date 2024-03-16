30c778ba3b52e22cf227d847d075af77e3efe05d
ansari2023 <imranahmedansari2021@gmail.com>
2024-03-16 21:37:19 +0500

    add

---
+++ Class-4/data-type.js
@@ -0,0 +1,5 @@
+var myName = "Imran Ahmed";
+var myAge = 36;
+var isJobless = false;
+console.log(myName, myAge, isJobless);
+console.log(myName.toUpperCase());
---
+++ Class-4/data-type.ts
@@ -0,0 +1,7 @@
+let myName: string = "Imran Ahmed";
+let myAge: number = 36;
+let isJobless: boolean = false;
+
+console.log(myName, myAge, isJobless,);
+
+console.log(myName.toUpperCase());
