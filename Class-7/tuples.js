6a8e72b2843f36fa8adf9548c4e5cbe9e40a33fa
ansari2023 <imranahmedansari2021@gmail.com>
2024-04-21 09:51:27 +0500

    edit

--- Class-7/tuples.js
+++ Class-7/tuples.js
@@ -1,5 +1,5 @@
 // create tuple 
-var ourTuple = [5, false, 'Coding God was here'];
+var ourTuple = [5, false, 'Coding was here'];
 // We have no type safety in our tuple for indexes 3+
 ourTuple.push('Something new and wrong');
 ourTuple.push(100);
