259107b963ad48802c7190c55794656a2aaf8f04
ansari2023 <imranahmedansari2021@gmail.com>
2024-05-02 01:22:19 +0500

    updated

---
+++ Class-8/table.js
@@ -0,0 +1,5 @@
+
+for (var i = 1; i <= 10; i++) {
+    var tableNumber = 25;
+    console.log(tableNumber, "x", i, "=", tableNumber * i); // 25x1=25 to 25x10=2500
+}
---
+++ Class-8/table.ts
@@ -0,0 +1,6 @@
+for (let i = 1; i <= 10; i++) {
+
+    let tableNumber : number = 25;
+    console.log(tableNumber, "x", i, "=", tableNumber * i);  // 25x1=25 to 25x10=2500
+
+}
